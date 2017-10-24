8472d4ad509b9e9b3c42172b7cd4e9d6fba47161
Simon Moe <moe.simon@gmail.com>
2017-10-25 00:20:41 +0200

    Using Pandas functions for dataAggregation

--- functions/dataAggregation.py
+++ functions/dataAggregation.py
@@ -11,20 +11,53 @@ USAGE:
 
 @Author: Simon Moe Sørensen, moe.simon@gmail.com
 """
-import numpy as np
 
 def aggregate_measurements(tvec, data, period):
+    
+    #Ignore cases
+    period = period.lower()
+    
+    #Join tvec and data
+    df = tvec.join(data)
     
     #Aggregate data for hourly consumption
     if period == "hour":
         
-        #Aggregate data
-        for i in range(max(tvec[:,3])):
-            
-            #Define the indicies of rows
-            inRow = np.where(tvec[:,3] == i)
-            
-            #Aggregate data
-            row = np.sum(data[inRow,:],axis=1)
+        #Group by hours
+        df_g = df.groupby(['year','month','day','hour'])
+                   
+        #Get the dataframe of aggregated data
+        df_a = df_g['zone1','zone2','zone3','zone4'].sum()
     
-    return (tvec_a, data_a)
+    elif period == "day":
+        
+        #Group by days
+        df_g = df.groupby(['year','month','day'])
+                   
+        #Get the dataframe of aggregated data
+        df_a = df_g['zone1','zone2','zone3','zone4'].sum()
+    
+    elif period == "month":
+        
+        #Group by days
+        df_g = df.groupby(['year','month'])
+                   
+        #Get the dataframe of aggregated data
+        df_a = df_g['zone1','zone2','zone3','zone4'].sum()
+        
+    elif period == "hour of the day":
+        
+        #Group by hour of the day
+        df_g = df.groupby(['hour'])
+                   
+        #Get the dataframe of aggregated data
+        df_a = df_g['zone1','zone2','zone3','zone4'].sum()
+    
+    #Define data and tvec seperately
+    data_a = df_a.iloc[:,6:10]
+    
+    tvec_a = df_a.iloc[:,0:6]
+        
+    return tvec_a, data_a
+
+tvec_a, data_a = aggregate_measurements(tvec,data,"hour of the day")
